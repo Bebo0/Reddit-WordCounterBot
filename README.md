f20220633b1c0ab1131acbbd54595a50a7ec34f8
Bebo Elhosary <beboelhosary@hotmail.com>
2017-12-25 21:35:51 -0800

    Cleaned code. Added dateInitial and dateEnd variables to parsePostTitles. Ordered functions in alphabetical order.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Reddit-WordCounterBot
+
+
+# USAGE: 1) type in a Reddit username and password in the praw.ini folder
+#        2) in terminal, cd to folder which contains project files
+#        3) type python reddit_bot.py
--- reddit_bot.py
+++ reddit_bot.py
@@ -3,36 +3,24 @@ import string
 import re
 from collections import Counter
 
-## USAGE: 1) type in a Reddit username and password in the praw.ini folder
-##        2) in terminal, cd to folder which contains project files
-##        3) type python reddit_bot.py
+# USAGE: 1) type in a Reddit username and password in the praw.ini folder
+#        2) in terminal, cd to folder which contains project files
+#        3) type python reddit_bot.py
 
+# VARIABLES:
 subRedditName = 'cryptocurrency'
 dictionary = Counter()
 
 
-"""
-void -> Reddit
-
-Logs us into Reddit and returns an object which allows us to interact with Reddit's API
-"""
-def authenticate():
-	print "Authenticating"
-	reddit = praw.Reddit('wordcounterbot',
-			user_agent = "Bebo's Word Counter")
-	print "Successfully authenticated as {}".format(reddit.user.me())
-	return reddit
-
-
-"""
-ArrayOfStrings -> void
-
-Adds all strings in given array to dictionary
-
-Arguments:
-	aos {ArrayOfStrings} -- contains all the words to be added to dictionary
-"""
 def addArrToDictionary(aos):
+	"""
+	ArrayOfStrings -> void
+
+	Adds all strings in given array to dictionary
+
+	Arguments:
+		aos {ArrayOfStrings} -- contains all the words to be added to dictionary
+	"""
 
 	for word in aos:
 		if word in dictionary:
@@ -40,6 +28,18 @@ def addArrToDictionary(aos):
 		else:
 			dictionary[word] = 1
 
+
+def authenticate():
+	"""
+	void -> Reddit
+
+	Logs us into Reddit and returns an object which allows us to interact with Reddit's API
+	"""
+	print "Authenticating"
+	reddit = praw.Reddit('wordcounterbot',
+			user_agent = "Bebo's Word Counter")
+	print "Successfully authenticated as {}".format(reddit.user.me())
+	return reddit
 
 
 def parseComments(reddit):
@@ -53,7 +53,7 @@ def parseComments(reddit):
 	"""
 	
 	print "Parsing comments..."
-	x = 5
+	x = 1000
 	for comment in reddit.subreddit(subRedditName).comments(limit=x):
 
 		# transforms all letters of the comment body to lowercase and transforms the comment from unicode to ascii for easier readability
@@ -61,26 +61,6 @@ def parseComments(reddit):
 
 		parsingHelper(strong)
 	print "Successfully parsed comments!"
-
-def parsePostTitles(reddit):
-	"""
-	Reddit -> void
-	
-	Parses all post titles from dateInitial to dateEnd in the given subreddit
-	
-	Arguments:
-		reddit {Reddit} -- [the Reddit object that allows us to interact with Reddit's API]
-	"""
-	
-	print "Parsing post titles..."
-
-	for comment in reddit.subreddit(subRedditName).submissions(1513998000, 1514150308):
-
-		strong = ''.join(comment.title).lower().encode('ascii','ignore')
-		parsingHelper(strong)
-
-	print "Successfully parsed post titles!"
-
 
 
 def parsingHelper(strong):
@@ -94,52 +74,49 @@ def parsingHelper(strong):
 	addArrToDictionary(aos)
 
 
+def parsePostTitles(reddit):
+	"""
+	Reddit -> void
+	
+	Parses all post titles from dateInitial to dateEnd in the given subreddit
+	
+	Arguments:
+		reddit {Reddit} -- [the Reddit object that allows us to interact with Reddit's API]
+	"""
+	
+	print "Parsing post titles..."
+	dateInitial = 1514178600 #December 25th, 2017 9:10pm PST. Convert time to UNIX time here: https://www.unixtimestamp.com/
+	dateEnd     = 1514265000 #December 26th, 2017 9:10pm PST
 
+	for comment in reddit.subreddit(subRedditName).submissions(dateInitial, dateEnd):
+
+		strong = ''.join(comment.title).lower().encode('ascii','ignore')
+		parsingHelper(strong)
+
+	print "Successfully parsed post titles!"
 
 
 def run_bot(reddit):
 	"""
 	Reddit -> void
 
-	Parses the comments and titles in the given subreddit, and split them up into invidivual.
+	Parses the comments and titles in the given subreddit, and adds certain strings found to dictionary.
 		
 	Arguments:
 		reddit {Reddit} -- [the Reddit object that allows us to interact with Reddit's API]
 	"""
-
-	
-	
 	parseComments(reddit)
 	parsePostTitles(reddit)
-	#print "Parsing comments..."
-	#for comment in reddit.subreddit('cryptocurrency').submissions(1513998000, 1514150308):
-
-		#strong = ''.join(comment.title).lower().encode('ascii','ignore')
-
-		#print "Parsing comments..."
-	# for comment in reddit.subreddit(subRedditName).comments(limit=10000):
-
-	# 	strong = ''.join(comment.body).lower().encode('ascii','ignore')
-
-	# 	allow = string.letters + string.digits + ' ' + '\'' + '-'
-	# 	lol = re.sub('[^%s]' % allow,'',strong)
-
-	# 	lol = lol.split()
-
-	# 	for word in lol:
-	# 		if word in dictionary:
-	# 			dictionary[word] += 1
-	# 		else:
-	# 			dictionary[word] = 1
-
-	print dictionary
-	#print type (dictionary)
 
 
 def main():
 	
 	reddit = authenticate()
 	run_bot(reddit)
+	print dictionary
 
 if __name__ == '__main__':
 	main()
+
+
+
